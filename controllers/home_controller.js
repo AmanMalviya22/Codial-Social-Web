8849c6bfa0def3105920e62a60267fb069495102
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-21 11:13:07 +0530

    created view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 //controller for home route and exported it
 module.exports.home = function(req, res) {
-   return res.end('home controller action is performed');
+   return res.render('home',{
+    title:"Home"
+   });
  };
  
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 8000;
 // Use express router
 app.use('/', require('./routes/index'));
 
+
+//set up out view engine
+app.set('view engine', 'ejs');
+//require views folder and use it
+app.set('views','./views');
 app.listen(port, function(err) {
   if (err) {
     console.log(`Error in listening ${err}`);
