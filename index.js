ed77811211629dc93b2970cc4d5fd3da9ea42627
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-21 09:03:25 +0530

    setup our router

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app=express();
 const port=8000;
 
 
-
-
+//use express router
+app.use('./', require('./routes/index'));
 
 app.listen(port,function(err){
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express=require('express');
+//require express router module for routing
+const routes=express.Router();
+console.log("routes get loaded");
+//exports the route 
+module.exports=routes;
