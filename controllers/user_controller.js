3fa314e658f732f02082016d02ab44e2c8c43d84
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-23 05:17:58 +0530

    created sign-in and sign-up pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -16,3 +16,15 @@ module.exports.signin=function(req,res){
     
     return res.render('user_sign_in',{title:"sign in"});
 }
+
+// sign up controller
+module.exports.create=function(req,res){
+    //later
+}
+
+
+// sign in controller
+module.exports.createSession=function(req,res){
+    // later
+}
+
