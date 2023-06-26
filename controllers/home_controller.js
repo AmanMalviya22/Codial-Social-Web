ff97cd91dbdc17f632977bd17cbdd7df3c5d3fb8
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-26 15:55:57 +0530

    display post and user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,41 @@
-module.exports.home = function(req, res){
-  console.log(req.cookies);
-  res.cookie('user_id', 25);
-  return res.render('home', {
-      title: "Home"
-  });
-}
+const Post = require("../models/post");
+module.exports.home = function (req, res) {
+  // console.log(req.cookies);
+  // res.cookie('user_id', 25);
 
+  // Post.find({})
+  //   .then((posts) => {
+  //     return res.render("home", {
+  //       title: "Home",
+  //       posts: posts,
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     // Handle the error
+  //     console.error(err);
+  //     // Return an appropriate response
+  //     return res.status(500).send("An error occurred.");
+  //   });
+
+
+
+
+  //populate the user of each post
+
+  Post.find({})
+    .populate('user')
+    .exec()
+    .then((posts) => {
+      return res.render("home", {
+        title: "Home",
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      // Handle the error
+      console.error(err);
+      // Return an appropriate response
+      return res.status(500).send("An error occurred.");
+    });
+};
 // module.exports.actionName = function(req, res){}
