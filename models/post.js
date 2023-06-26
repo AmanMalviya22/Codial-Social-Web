2908345fa23d1d9f579b07073a8948411cb07a5e
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-26 16:26:48 +0530

    created comment schema

--- models/post.js
+++ models/post.js
@@ -9,6 +9,10 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref:'User'
+    },
+    comments: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'comment'
     }
     
 }, {
