da519166c2586eb3cf4e343b16637a4e8cca66c3
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-26 20:30:04 +0530

    added comment to the database

--- models/comment.js
+++ models/comment.js
@@ -1,24 +1,23 @@
-const mongoose = require("mongoose");
-const commentSchema = mongoose.Schema(
-  {
-    content: {
-      type: String,
-      required: true,
-    },
+const mongoose = require('mongoose');
 
-    //comment belongs to a user
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    // comment belongs to a user
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     },
     post: {
-      type: mongoose.Schema.ObjectId,
-      ref: "Post",
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-const Comment=mongoose.model('Comment',commentSchema);
-module.exports=Comment;
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+},{
+    timestamps: true
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
--- models/post.js
+++ models/post.js
@@ -1,25 +1,57 @@
-const mongoose = require('mongoose');
-
-const postSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true,
+// const mongoose = require('mongoose');
+// const comment=require('./comment');
+// const postSchema = new mongoose.Schema({
+//     content: {
+//         type: String,
+//         required: true,
         
+//     },
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref:'User'
+//     },
+//     comments: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref:'comment'
+//     }
+    
+// }, {
+//     timestamps: true
+// });
+
+
+// const Post = mongoose.model('Post', postSchema);
+
+// module.exports =Post;
+
+
+const mongoose = require('mongoose');
+const commentSchema = require('./comment');
+
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
-    comments: {
+    comments: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref:'comment'
-    }
-    
-}, {
-    timestamps: true
-});
-
+        ref: 'Comment',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
 
-module.exports =Post;
+
+
