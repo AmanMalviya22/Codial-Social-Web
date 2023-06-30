a654681d6557388dbef46c13aa1eea60e1f8459a
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-30 10:15:27 +0530

    styled friend section to the right side of screen using scss || learn and implement promises and async await to make code neat and clean

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -57,25 +57,28 @@ module.exports.signIn = function(req, res){
 
 // get the sign up data
 module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
-        return res.redirect('back');
-    }
+  if (req.body.password != req.body.confirm_password){
+      return res.redirect('back');
+  }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+  User.findOne({email: req.body.email})
+      .then(user => {
+          if (!user){
+              return User.create(req.body);
+          } else {
+              return Promise.reject('User already exists');
+          }
+      })
+      .then(user => {
+          return res.redirect('/users/sign-in');
+      })
+      .catch(error => {
+          console.log('Error in creating user while signing up:', error);
+          return res.redirect('back');
+      });
+};
 
-        if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
-
-                return res.redirect('/users/sign-in');
-            })
-        }else{
-            return res.redirect('back');
-        }
-
-    });
-}
+    
 
 
 // sign in and create a session for the user
