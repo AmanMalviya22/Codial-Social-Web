57977b39592de122eb3a8d2934625fdf1b789ec5
AmanMalviya22 <amanaman26802@gmail.com>
2023-07-02 17:51:46 +0530

    saving avatar form params

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -19,13 +19,30 @@ module.exports.profile = async function (req, res) {
 module.exports.update = async function (req, res) {
   try {
     if (req.user.id == req.params.id) {
-      await User.findByIdAndUpdate(req.params.id, req.body);
-      return res.redirect('back');
+      // await User.findByIdAndUpdate(req.params.id, req.body);
+      // return res.redirect('back');
+
+      let user=await User.findById(req.params.id);
+      User.uploadedAvatar(req,res,function(err){
+        if(err){console.log('***** Multer Error :',err)}
+        // console.log(req.file);
+        user.name=req.body.name;
+        user.email=req.body.email;
+        if(req.file){
+          //this is saving the path of the uploaded file into the avatar field in the user
+          user.avatar=User.avatarPath+'/'+req.file.filename;
+        }
+        user.save();
+        return res.redirect('back');
+
+
+      });
     } else {
       return res.status(401).send('Unauthorized');
     }
   } catch (err) {
-    console.error('Error updating user:', err);
+    // console.error('Error updating user:', err);
+    req.flash('error', err);
     return res.redirect('back');
   }
 };
