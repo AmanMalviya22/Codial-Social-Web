76c0a7f8fe4d9bf335a10f2856b3a4a3578cd076
AmanMalviya22 <amanaman26802@gmail.com>
2023-07-03 12:13:19 +0530

    deteled post without authentication using api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,53 @@
-module.exports.index=function(req,res){
-    return res.json(200,{
-        message:"list of posts",
-        posts:[]
-    })
-}
+const Post = require("../../../models/post");
+const Comment=require('../../../models/comment');
+module.exports.index = async function (req, res) {
+  const posts = await Post.find({})
+    .sort(`-createdAt`)
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
+
+  return res.json(200, {
+    message: "list of posts",
+    posts: posts,
+  });
+};
+
+
+
+module.exports.destroy = async function(req, res) {
+    try {
+      const post = await Post.findById(req.params.id);
+  
+    //   if (!post) {
+    //     console.error('Post not found');
+    //     return res.redirect('back');
+    //   }
+  
+    //   if (post.user.equals(req.user._id)) {
+        await post.deleteOne(); // Updated: Replaced remove() with deleteOne()
+        await Comment.deleteMany({ post: req.params.id });
+        return res.json(200,{
+            message:"Post and associated comments deleted successfullyy"
+        });
+        // req.flash('success','post and associated comments are deleted');
+        return res.redirect('back');
+    //   } else {
+        req.flash('error','you can not delete the post');
+        return res.redirect('back');
+      }
+    // } 
+    catch (err) {
+  
+    //   req.flash('error',err);
+  
+      console.error('Error finding or deleting post:', err);
+      return res.json(500, {
+        message:"internal server error"
+      })
+    }
+  };
