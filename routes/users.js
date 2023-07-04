58c17d4bd75dc9ff50104bbe0fd398bf8af54a8c
AmanMalviya22 <amanaman26802@gmail.com>
2023-07-04 15:00:10 +0530

    Added Google Sign in Authentication

--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,13 @@ router.post('/create-session', passport.authenticate(
 
 router.get('/sign-out', usersController.destroySession);
 
+
+
+// it should be profile not profille
+router.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}));
+router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/sign-in'}),usersController.createSession);
+
+
+
+
 module.exports = router;
