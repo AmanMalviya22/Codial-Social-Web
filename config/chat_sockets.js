3b8f1a34303af2c99a4cf19b4571e298d0bc4b16
AmanMalviya22 <amanaman26802@gmail.com>
2023-07-09 17:31:05 +0530

    biggining the initial connection

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,18 @@
-  module.exports.chatSockets=function(socketServer){
 
-  }
+module.exports.chatSockets = function(socketServer){
+  // let io = require('socket.io')(socketServer);
+
+  let io = require('socket.io')(socketServer, {cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]}});
+
+  io.sockets.on('connection', function(socket){
+      console.log('new connection received', socket.id);
+
+      socket.on('disconnect', function(){
+          console.log('socket disconnected!');
+      });
+
+  });
+
+}
