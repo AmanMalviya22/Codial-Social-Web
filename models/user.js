ba4eba05f469dafa5696c54cd857dce49bf8e5ba
AmanMalviya22 <amanaman26802@gmail.com>
2023-07-09 15:08:33 +0530

    created _chat_box.ejs

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const multer=require('multer');
 const path=require('path');
 const AVATAR_PATH=path.join('/uploads/users/avatars');
+const friendshipSchema=require('./friendship');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -20,7 +21,13 @@ const userSchema = new mongoose.Schema({
     avatar:{
         type:String,
 
-    }
+    },
+    friendships:[
+        {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Friendship'
+        }
+    ]
 }, {
     timestamps: true
 });
