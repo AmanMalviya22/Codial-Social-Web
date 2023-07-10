55c5e511a11ebb3b354919981b6d8bc3de3b97f8
AmanMalviya22 <amanaman26802@gmail.com>
2023-07-10 17:16:29 +0530

    time to create and join chat

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,18 +1,24 @@
-
-module.exports.chatSockets = function(socketServer){
+module.exports.chatSockets = function (socketServer) {
   // let io = require('socket.io')(socketServer);
 
-  let io = require('socket.io')(socketServer, {cors: {
-    origin: "*", 
-    methods: ["GET", "POST"]}});
-
-  io.sockets.on('connection', function(socket){
-      console.log('new connection received', socket.id);
-
-      socket.on('disconnect', function(){
-          console.log('socket disconnected!');
-      });
-
+  let io = require("socket.io")(socketServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
   });
 
-}
+  io.sockets.on("connection", function (socket) {
+    console.log("new connection received", socket.id);
+
+    socket.on("disconnect", function () {
+      console.log("socket disconnected!");
+    });
+
+    socket.on("join_room", function (data) {
+      console.log("joining request receiver", data);
+      socket.join(data.chatroom);
+      io.in(data.chatroom).emit('user_joined', data);
+    });
+  });
+};
