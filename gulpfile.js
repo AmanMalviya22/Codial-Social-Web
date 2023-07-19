b210f0658031c4741ab53b69dfecd554daf9cd2c
Aman Malviya <amanaman26802@gmail.com>
2023-07-19 10:37:43 +0530

    skip

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+
+require = require('esm')(module);
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev2');
+
+gulp.task('css', function() {
+  console.log('Minifying CSS...');
+
+  gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./assets/css'));
+
+  return gulp.src('./assets/**/*.css')
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+      cwd: 'public',
+      merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+});
